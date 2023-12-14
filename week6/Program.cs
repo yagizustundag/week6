a4ba5bdf47aa669206611fd6359073afdbd16517
Your Name <you@example.com>
2023-12-14 14:54:12 +0300

    week6

---
+++ week6/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+string[] FruitBasket = { "Apples", "Bananas", "Oranges" };
+
+Console.WriteLine($"There are {FruitBasket.Length} fruit in basket.");
+
+//Console.WriteLine(FruitBasket[0]);
+//Console.WriteLine(FruitBasket[1]);
+//Console.WriteLine(FruitBasket[2]);
+
+for (int i = 0; i < FruitBasket.Length; i++)
+{
+    Console.WriteLine(FruitBasket[i]);
+}
+
+FruitBasket[0] = "Pine-apple";
+FruitBasket[2] = "Peaches";
+
+//Console.WriteLine(FruitBasket[0]);
+//Console.WriteLine(FruitBasket[1]);
+//Console.WriteLine(FruitBasket[2]);
+
+for (int i = 0; i < FruitBasket.Length; i++)
+{
+    Console.WriteLine(FruitBasket[i]);
+}
+
---
+++ week62/Program.cs
@@ -0,0 +1,27 @@
+﻿string MyString = "Erasmuse";
+
+Console.WriteLine(MyString.Length);
+
+Console.WriteLine(MyString[0]);
+Console.WriteLine(MyString[MyString.Length - 1]);
+
+// a-counter
+
+//int counter = 0;
+
+//for (int i = 0; i < MyString.Length; i++)
+//{
+//    if (MyString[i] == 'a')
+//    {
+//        counter++;
+//    }
+//}
+
+//Console.WriteLine($"Your word have {counter} a letters.");
+
+//replace 
+// a-4
+
+string UpdatedWord = MyString.Replace('a', '4').Replace('e', '3'); ;
+
+Console.WriteLine(UpdatedWord);
